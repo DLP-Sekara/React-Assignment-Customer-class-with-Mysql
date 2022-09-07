32d04e41e00c44fc02694e1397f9ce290fe59836
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-09-07 07:08:32 +0530

    content updated

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Customer from "../pages/Customer/index";
+import Item from "../pages/Item/item"
 
 function App() {
     return(
         <Routes>
             <Route exact path='/' element={<Customer/>}/>
-
+            <Route path="item" element={<Item/>}/>
         </Routes>
     );
 }
---
+++ src/pages/Item/item.jsx
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+import { Grid, Typography } from "@mui/material";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import GDSEButton from '../../components/common/Button';
+import CustomerService from "../../services/CustomerService";
+import GDSESnackBar from "../../components/common/SnackBar";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import EditIcon from '@mui/icons-material/Edit';
+class Item extends Component{
+
+}
+export  default Item;
